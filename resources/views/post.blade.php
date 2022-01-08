94588c8a87d5e1d7c846578d337ca8b5eaf9c7ce
Damion Mullins <Damiontmullins@gmail.com>
2022-01-08 11:57:57 -0600

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<title> my blog </title>
+
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+<article>
+
+<?= $post; ?>
+</article>
+
+<a href='/'>go back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+
+<title> my blog </title>
+
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+    <article>
+
+        <h1><a href="/posts/my-first-post">My first post </a></h1>
+        <p>
+            well you see what had happened was 
+        </p>
+
+    </article>
+    <article>
+
+        <h1><a href="/posts/my-second-post">My second post </a></h1>
+        <p>
+            well you see what hppened was this. i mean okay 
+        </p>
+
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My third post </a></h1>
+        <p>
+            well you see what hppened was this. i mean okay 
+    
+        </p>
+
+    </article>
+
+   
+</body>
+
+
